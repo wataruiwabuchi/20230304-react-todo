a36ec31eb4d17bdf7f380903ffe57644559e3e45
a <wataru@DESKTOP-US95HHS.localdomain>
2023-03-05 21:05:52 +0900

    temp

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './TodoList'
-import TodoInput from './TodoInput';
+
+type TodoItem = {
+    title: string;
+    isCompleted: boolean;
+};
+
+enum DiplayedStatus {
+    ALL,
+    ACTIVE,
+    COMPLETED
+}
+
+const returnDisplayedFilterFunction = (displayedStatus: DiplayedStatus) => {
+    switch (displayedStatus) {
+        case DiplayedStatus.ALL:
+            return () => true;
+        case DiplayedStatus.ACTIVE:
+            return (todoItem: TodoItem) => !todoItem.isCompleted;
+        case DiplayedStatus.COMPLETED:
+            return (todoItem: TodoItem) => todoItem.isCompleted;
+    }
+}
 
 function App() {
+
+    const [currentFilter, setCurrentFilter] = useState<DiplayedStatus>(DiplayedStatus.ALL);
+    const [inputText, setInputText] = useState<string>('')
+    const [todoList, setTodoList] = useState<TodoItem[]>(
+        [
+            { title: 'Learn React', isCompleted: false },
+            { title: 'Learn React2', isCompleted: false },
+        ]
+    );
+
+    const handleInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    }
+
+    const addItem = () => {
+        if (inputText === '') {
+            return;
+        }
+
+        let newTodoList = [...todoList];
+        newTodoList.push({
+            title: inputText,
+            isCompleted: false
+        });
+        setTodoList(newTodoList);
+        setInputText('');
+    }
+
+    const deleteItem = (index: number) => {
+        let newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        setTodoList(newTodoList);
+    }
+
+    const toggleItemCompletion = (index: number) => {
+        let newTodoList = [...todoList];
+        newTodoList[index].isCompleted = !newTodoList[index].isCompleted;
+        setTodoList(newTodoList);
+    }
+
   return (
     <div className="App">
       <a href="https://codingthesmartway.com" target="_blank" rel="noreferrer noopener"><img src={'/img/CTSWLogo.png'} alt="svelte logo" className="logo" /></a>
       <h2>React Todo App</h2>
-      {/* <input type="text" className="todo-input" placeholder="Insert todo item ..." bind:value={newTodoTitle} on:keydown={addTodo}> */}
-      <TodoInput></TodoInput>
-      <div className="inner-container">
-          {/*
-          <div><label><input className="inner-container-input" type="checkbox" on:change={checkAllTodos}>Check All</label></div>
-          <div>{todosRemaining} items left</div>
-          */}
-          <TodoList />
+      <div>
+        <label>
+            <input type="text" className="todo-input" placeholder="Insert todo item ..." value={inputText} onChange={handleInputText}></input>
+            <button onClick={addItem}>Submit</button>
+        </label>
       </div>
       <div className="inner-container">
-          {/*
+          {
+            todoList.filter(returnDisplayedFilterFunction(currentFilter)).map((todoItem, index) => {
+              return (
+                <div>
+                    <label>
+                        <input type="checkbox" checked={todoItem.isCompleted} onClick={() => toggleItemCompletion(index)} />
+                        <p style={todoItem.isCompleted ? { textDecoration: "line-through" } : {}}>
+                            {todoItem.title}
+                        </p>
+                        <button onClick={() => deleteItem(index)}>Delete</button>
+                    </label>
+                </div>
+              );
+            })
+          }
+      </div>
+      <div className="inner-container">
+          {
           <div>
-              <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">All</button>
-              <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Active</button>
-              <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completed</button>
+              <button onClick={() => setCurrentFilter(DiplayedStatus.ALL)}>ALL</button>
+              <button onClick={() => setCurrentFilter(DiplayedStatus.ACTIVE)}>ACTIVE</button>
+              <button onClick={() => setCurrentFilter(DiplayedStatus.COMPLETED)}>Completed</button>
           </div>
-          <div>
-              <button on:click={clearCompleted}>Clear Completed</button>
-          </div>
-          */}
+          }
       </div>
   </div>
   );
 }
 
 export default App;
+
+export type { TodoItem };
